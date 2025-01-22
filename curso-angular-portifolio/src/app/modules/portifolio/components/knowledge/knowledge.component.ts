6e84d5f6b145589426567ab893ea6962c94a2ee4
BrendonAraujo <brendong.araujo@gmail.com>
2025-01-22 11:01:30 -0300

    #ADD - Knowledge component

---
+++ curso-angular-portifolio/src/app/modules/portifolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal } from '@angular/core';
+import { IKnowledge } from '../../interface/Iknowledge';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: '/assets/icons/knowledge/html5.svg',
+      alt: "HTML5 Icon"
+    },
+    {
+      src: '/assets/icons/knowledge/css3.svg',
+      alt: "CSS3 Icon"
+    },
+    {
+      src: '/assets/icons/knowledge/javascript.svg',
+      alt: "Javascript Icon"
+    },
+    {
+      src: '/assets/icons/knowledge/angular.svg',
+      alt: "Angular Icon"
+    },
+    {
+      src: '/assets/icons/knowledge/nodejs.svg',
+      alt: "NodeJs Icon"
+    }
+  ]);
+}
